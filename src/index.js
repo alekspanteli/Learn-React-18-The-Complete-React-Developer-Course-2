1e9d599423bde810ee681dc6904508836daa8211
alekspanteli <alekspanteli@rambler.ru>
2022-08-16 16:28:56 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from '/App';
 
-import App from './App';
+const root = ReactDom.createRoot(document.getElementById('root'));
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+root.render(<App />);
